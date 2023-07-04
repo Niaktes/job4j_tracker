6263b97cebaf93aea2f4c04f2352d79db37ce4b9
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-07-04 21:34:14 +0300

    3.3.1.1. Lombok. Настройка IDEA [#504888] - equals and hashCode methods returned.

--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker.model;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import lombok.Data;
 
 @Data
@@ -29,6 +30,23 @@ public class Item {
     @Override
     public String toString() {
         return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
 }
