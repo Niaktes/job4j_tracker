81c97e7246f8f34e64cbdbcaf315059f92413ae7
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-08-29 10:31:48 +0300

    3.4.5.1. Интеграционное тестирование с Hibernate [#6875] - tests issue fixed

--- src/test/java/ru/job4j/tracker/store/HbmTrackerTest.java
+++ src/test/java/ru/job4j/tracker/store/HbmTrackerTest.java
@@ -1,74 +1,76 @@
 package ru.job4j.tracker.store;
 
+import org.junit.*;
+
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import ru.job4j.tracker.model.Item;
 
-class HbmTrackerTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-    @AfterEach
-    void tableClean() {
+public class HbmTrackerTest {
+
+    @After
+    public void tableClean() {
         try (var tracker = new HbmTracker()) {
             tracker.findAll().forEach(item -> tracker.delete(item.getId()));
         }
     }
 
     @Test
-    void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() throws Exception {
+    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
         try (var tracker = new HbmTracker()) {
             Item item = tracker.add(new Item("test"));
-            Assertions.assertEquals(item, tracker.findById(item.getId()));
+            assertEquals(item, tracker.findById(item.getId()));
         }
     }
 
     @Test
-    void whenAddAndDeleteThenFindByIdIsNull() {
+    public void whenAddAndDeleteThenFindByIdIsNull() {
         try (var tracker = new HbmTracker()) {
             Item item = tracker.add(new Item("test"));
             tracker.delete(item.getId());
-            Assertions.assertNull(tracker.findById(item.getId()));
+            assertNull(tracker.findById(item.getId()));
         }
     }
 
     @Test
-    void whenAddAndReplaceThenFindByIdSecondItem() {
+    public void whenAddAndReplaceThenFindByIdSecondItem() {
         try (var tracker = new HbmTracker()) {
             Item itemOne = tracker.add(new Item("One"));
             int id = itemOne.getId();
             Item itemTwo = new Item("Two");
             itemTwo.setId(id);
             tracker.replace(id, itemTwo);
-            Assertions.assertEquals(itemTwo, tracker.findById(id));
+            assertEquals(itemTwo, tracker.findById(id));
         }
     }
 
     @Test
-    void whenAddTwoAndFindAllThenGetTwoItems() {
+    public void whenAddTwoAndFindAllThenGetTwoItems() {
         try (var tracker = new HbmTracker()) {
             Item itemOne = tracker.add(new Item("One"));
             Item itemTwo = tracker.add(new Item("Two"));
-            Assertions.assertEquals(List.of(itemOne, itemTwo), tracker.findAll());
+            assertEquals(List.of(itemOne, itemTwo), tracker.findAll());
         }
     }
 
     @Test
-    void whenAddItemAndFindByNameThenGetItem() {
+    public void whenAddItemAndFindByNameThenGetItem() {
         try (var tracker = new HbmTracker()) {
             Item item = tracker.add(new Item("One"));
-            Assertions.assertEquals(List.of(item), tracker.findByName(item.getName()));
+            assertEquals(List.of(item), tracker.findByName(item.getName()));
         }
     }
 
     @Test
-    void whenAddMultipleAndDeleteOneThenFindAllWithoutDeletedItem() {
+    public void whenAddMultipleAndDeleteOneThenFindAllWithoutDeletedItem() {
         try (var tracker = new HbmTracker()) {
             Item itemOne = tracker.add(new Item("One"));
             Item itemTwo = tracker.add(new Item("Two"));
             Item itemThree = tracker.add(new Item("Three"));
             tracker.delete(itemTwo.getId());
-            Assertions.assertEquals(List.of(itemOne, itemThree), tracker.findAll());
+            assertEquals(List.of(itemOne, itemThree), tracker.findAll());
         }
     }
 
